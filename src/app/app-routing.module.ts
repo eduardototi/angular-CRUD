7896a58c86cc0a5bc351986d397a4bd3a75065e4
Pedro Eduardo <eduardototi@gmail.com>
2021-07-10 13:15:35 -0300

    10/07/2021 class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent
+  },
+  {
+    path: "products/update/:id",
+    component: ProductUpdateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
